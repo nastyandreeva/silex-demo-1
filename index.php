e0a2bc9ff161ea7d65efc334495d7c3973e1fbe6
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-29 12:20:29 +0300

    third

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   });
   
   $app->get('/add/{n1}/{n2}', function ($n1, $n2) use($app) {
-    return '<h2>Сумма: </h2><h3>' . ($n1 + $n2) . '</h3'; 
+    return '<h2>Сумма: </h2><h3>' . ($n1 + $n2) . '</h3>'; 
   })->after($hdrs);
 
   $app->error(function ($e) use($app) {
